f5ccc830368a027c3d1da30b6c7ff2fce2764109
kerim <krmksmsite@gmail.com>
2022-08-13 18:44:47 +0300

    project completed

--- script.js
+++ script.js
@@ -5,13 +5,72 @@ const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=0d18624755
 
 const form = document.querySelector("#form")
 const search = document.querySelector(".search")
+const main = document.querySelector("main")
 
-getMovies(API_URL)
 
- async function getMovies(url) {
+getMovies(API_URL);
+
+async function getMovies(url) {
 
      const res = await fetch(url)
      const data = await res.json()
 
-     console.log(data.results)
+     showMovies(data.results)
 }
+
+function showMovies(movies){
+
+    main.innerHTML=""
+
+    movies.forEach((movie) => {
+
+      const { title, overview, poster_path, vote_average } = movie;
+
+      const div = document.createElement("div");
+
+      div.innerHTML = `<div class="movie">
+        <img src="${IMG_PATH + poster_path}" alt="">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class=${setColor(vote_average)}>${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h4>Overview</h4>
+            <p>${overview}</p>
+        </div>
+    </div>`;
+
+      main.append(div);
+      
+    });
+
+}
+
+function setColor(score) {
+
+    if(score >= 7){
+        return "green"
+    }
+    if(score >= 5){
+        return "orange"
+    }
+    else{
+        return "red"
+    }
+}
+
+form.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    const searchText = search.value
+    
+    if(searchText){
+        getMovies(SEARCH_URL + searchText)
+        search.value = ""
+    }
+    else {
+        alert("Please Enter a Movie Name")
+    }
+})
+
